b19f63b51290573359d719497fcae13e39b2cdd5
europanite <europanite@gmail.com>
2026-06-13 09:20:12 +0900

    fix/scripts

--- scripts/make_md.sh
+++ scripts/make_md.sh
@@ -1,4 +1,5 @@
 # !/bin/bash
-source ../env/bin/activate
-python3 ../make_md.py -r "$@"
+SCRIPT_DIR="$(dirname "$(dirname "${BASH_SOURCE[0]}")")"
+source ${SCRIPT_DIR}/env/bin/activate
+python3 ${SCRIPT_DIR}/make_md.py -r .
 deactivate
